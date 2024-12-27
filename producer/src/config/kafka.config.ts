e1265eca0e701bb595ddd1dd08d109143bf725f6
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-12-27 14:11:41 +0530

    added advertised listener of kafka broker 3

--- producer/src/config/kafka.config.ts
+++ producer/src/config/kafka.config.ts
@@ -2,5 +2,5 @@ import { Kafka } from 'kafkajs'
 
 export const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['localhost:19092', 'localhost:29092'], // use advertise listeners
+  brokers: ['localhost:19092', 'localhost:29092', 'localhost:39092'], // use advertise listeners
 })
